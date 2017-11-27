b5b0f778122a7c003045c9e295c25ce13b25a2f6
Anton Gerasimov <me@untone.net>
2017-11-27 23:10:17 +0300

    Added help project files. Get rid off some warning when building solution

--- test/Starcounter.Linq.Tests/LinqTests.cs
+++ test/Starcounter.Linq.Tests/LinqTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Xunit;
 using static Starcounter.Linq.DbLinq;
 using static Starcounter.Linq.Tests.Utils;
@@ -114,6 +115,8 @@ namespace Starcounter.Linq.Tests
                 Sql(() => Objects<Person>().FirstOrDefault(p => p is Employee)));
         }
 
+#pragma warning disable CS0184 // 'is' expression's given expression is never of the provided type
+        // ReSharper disable IsExpressionAlwaysTrue
         [Fact]
         public void TypeIs_Nested()
         {
@@ -127,19 +130,23 @@ namespace Starcounter.Linq.Tests
             Assert.Equal("SELECT E FROM \"Starcounter\".\"Linq\".\"Tests\".\"Employee\" E WHERE ((E.\"Department\".\"Company\" IS \"Starcounter\".\"Linq\".\"Tests\".\"Company\")) FETCH 1",
                 Sql(() => Objects<Employee>().FirstOrDefault(d => d.Department.Company is Company)));
         }
+        // ReSharper restore IsExpressionAlwaysTrue
+
+        // ReSharper disable IsExpressionAlwaysFalse
+        [Fact]
+        public void TypeIs_ReservedWordType()
+        {
+            Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"Tests\".\"Person\" P WHERE ((P IS \"Starcounter\".\"Linq\".\"Tests\".\"Unknown\")) FETCH 1",
+                Sql(() => Objects<Person>().FirstOrDefault(p => p is Unknown)));
+        }
+        // ReSharper restore IsExpressionAlwaysFalse
+#pragma warning restore CS0184 // 'is' expression's given expression is never of the provided type
 
         [Fact]
         public void TypeIsNot()
         {
             Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"Tests\".\"Person\" P WHERE (NOT (P IS \"Starcounter\".\"Linq\".\"Tests\".\"Employee\")) FETCH 1",
                 Sql(() => Objects<Person>().FirstOrDefault(p => !(p is Employee))));
-        }
-
-        [Fact]
-        public void TypeIs_ReservedWordType()
-        {
-            Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"Tests\".\"Person\" P WHERE ((P IS \"Starcounter\".\"Linq\".\"Tests\".\"Unknown\")) FETCH 1",
-                Sql(() => Objects<Person>().FirstOrDefault(p => p is Unknown)));
         }
 
         [Fact]
