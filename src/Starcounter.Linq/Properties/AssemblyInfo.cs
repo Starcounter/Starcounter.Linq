144fd622683005207bbf60793ab9f25cf4c9d4f3
Anton Gerasimov <me@untone.net>
2017-11-27 23:23:30 +0300

    Added .nuspec file. Fixed project file

--- src/Starcounter.Linq/Properties/AssemblyInfo.cs
+++ src/Starcounter.Linq/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Starcounter.Linq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LINQ Provider for Starcounter DB")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Starcounter")]
 [assembly: AssemblyProduct("Starcounter.Linq")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,15 +21,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("df08e065-aaf1-42db-a92e-d288154f5663")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
