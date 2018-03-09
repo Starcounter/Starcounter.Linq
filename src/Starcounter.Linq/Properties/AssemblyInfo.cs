bc31923ba5eae0a9ab408f99c343442d445aff9c
Anton Gerasimov <me@untone.net>
2018-03-10 00:54:54 +0300

    1.0.1

--- src/Starcounter.Linq/Properties/AssemblyInfo.cs
+++ src/Starcounter.Linq/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("df08e065-aaf1-42db-a92e-d288154f5663")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
