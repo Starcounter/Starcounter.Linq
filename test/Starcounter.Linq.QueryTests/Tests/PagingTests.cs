e180aa627e1280476465d64823cb35f3ec49fb32
Anton Gerasimov <me@untone.net>
2018-07-12 00:56:32 +0300

    Support GetObjectNo in queries (Where and OrderBy)
    For https://github.com/Starcounter/Starcounter.Linq/issues/47

--- test/Starcounter.Linq.QueryTests/Tests/PagingTests.cs
+++ test/Starcounter.Linq.QueryTests/Tests/PagingTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using static Starcounter.Linq.DbLinq;
 
@@ -38,6 +39,36 @@ namespace Starcounter.Linq.QueryTests
                     ? CompileQuery(() => Objects<Person>().Skip(1))().ToList()
                     : Objects<Person>().Skip(1).ToList();
                 Assert.Equal(1, persons.Count);
+            }).Wait();
+        }
+
+        [Theory]
+        [InlineData(Mode.AdHoc)]
+        [InlineData(Mode.CompiledQuery)]
+        public void OrderBy(Mode mode)
+        {
+            Scheduling.RunTask(() =>
+            {
+                List<Person> people = mode == Mode.CompiledQuery
+                    ? CompileQuery(() => Objects<Person>().OrderBy(x => x.Age))().ToList()
+                    : Objects<Person>().OrderBy(x => x.Age).ToList();
+                Assert.Equal(2, people.Count);
+                Assert.True(people[0].GetObjectNo() < people[1].GetObjectNo());
+            }).Wait();
+        }
+
+        [Theory]
+        [InlineData(Mode.AdHoc)]
+        [InlineData(Mode.CompiledQuery)]
+        public void OrderBy_ObjectNo(Mode mode)
+        {
+            Scheduling.RunTask(() =>
+            {
+                List<Person> people = mode == Mode.CompiledQuery
+                    ? CompileQuery(() => Objects<Person>().OrderByDescending(x => x.GetObjectNo()))().ToList()
+                    : Objects<Person>().OrderByDescending(x => x.GetObjectNo()).ToList();
+                Assert.Equal(2, people.Count);
+                Assert.True(people[0].GetObjectNo() > people[1].GetObjectNo());
             }).Wait();
         }
     }
