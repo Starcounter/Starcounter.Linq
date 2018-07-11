da66f3115c1696be8916cb374166750d7f45efce
Anton Gerasimov <me@untone.net>
2018-07-11 14:18:03 +0300

    Refactoring in tests

--- test/Starcounter.Linq.SqlTests/MiscellaneousTests.cs
+++ test/Starcounter.Linq.SqlTests/MiscellaneousTests.cs
@@ -11,62 +11,62 @@ namespace Starcounter.Linq.SqlTests
         public void WhereEquals_Multiple()
         {
             var name = "XXX";
-            Assert.Equal((string)"SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE ((P.\"Name\" = ?)) AND ((P.\"Age\" = ?))",
-                (string)Sql(() => Objects<Person>().Where(p => p.Name == name).Where(p => p.Age == 1)));
+            Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE ((P.\"Name\" = ?)) AND ((P.\"Age\" = ?))",
+                Sql(() => Objects<Person>().Where(p => p.Name == name).Where(p => p.Age == 1)));
         }
 
         [Fact]
         public void WhereEquals_Or()
         {
             var name = "XXX";
-            Assert.Equal((string)"SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE (((P.\"Name\" = ?) OR (P.\"Age\" = ?)))",
-                (string)Sql(() => Objects<Person>().Where(p => p.Name == name || p.Age == 1)));
+            Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE (((P.\"Name\" = ?) OR (P.\"Age\" = ?)))",
+                Sql(() => Objects<Person>().Where(p => p.Name == name || p.Age == 1)));
         }
 
         [Fact]
         public void WhereEquals_And()
         {
             var name = "XXX";
-            Assert.Equal((string)"SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE (((P.\"Name\" = ?) AND (P.\"Age\" = ?)))",
-                (string)Sql(() => Objects<Person>().Where(p => p.Name == name && p.Age == 1)));
+            Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE (((P.\"Name\" = ?) AND (P.\"Age\" = ?)))",
+                Sql(() => Objects<Person>().Where(p => p.Name == name && p.Age == 1)));
         }
 
         [Fact]
         public void NestedValue_Equals_And()
         {
             var age = 123;
-            Assert.Equal((string)"SELECT E FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Employee\" E WHERE (((E.\"Department\".\"Company\".\"Name\" = ?) AND (E.\"Age\" > ?)))",
-                (string)Sql(() => Objects<Employee>().Where(p => p.Department.Company.Name == "XXX" && p.Age > age)));
+            Assert.Equal("SELECT E FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Employee\" E WHERE (((E.\"Department\".\"Company\".\"Name\" = ?) AND (E.\"Age\" > ?)))",
+                Sql(() => Objects<Employee>().Where(p => p.Department.Company.Name == "XXX" && p.Age > age)));
         }
 
         [Fact]
         public void ReservedWordField_ValueEquals()
         {
-            Assert.Equal((string)"SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE ((P.\"Limit\" = ?))",
-                (string)Sql(() => Objects<Person>().Where(p => p.Limit == 5)));
+            Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE ((P.\"Limit\" = ?))",
+                Sql(() => Objects<Person>().Where(p => p.Limit == 5)));
         }
 
         [Fact]
         public void ReservedWordField_NestedValueEquals()
         {
-            Assert.Equal((string)"SELECT E FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Employee\" E WHERE ((E.\"Department\".\"Company\".\"Index\" = ?))",
-                (string)Sql(() => Objects<Employee>().Where(p => p.Department.Company.Index == 0)));
+            Assert.Equal("SELECT E FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Employee\" E WHERE ((E.\"Department\".\"Company\".\"Index\" = ?))",
+                Sql(() => Objects<Employee>().Where(p => p.Department.Company.Index == 0)));
         }
 
         [Fact]
         public void EnumerableContains()
         {
             var ages = new[] { 41, 42, 43 };
-            Assert.Equal((string)"SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE (((P.\"Age\" = ?) OR (P.\"Age\" = ?) OR (P.\"Age\" = ?)))",
-                (string)Sql(() => Objects<Person>().Where(p => ages.Contains(p.Age))));
+            Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE (((P.\"Age\" = ?) OR (P.\"Age\" = ?) OR (P.\"Age\" = ?)))",
+                Sql(() => Objects<Person>().Where(p => ages.Contains(p.Age))));
         }
 
         [Fact]
         public void EnumerableNotContains()
         {
             var ages = new[] { 41, 42, 43 };
-            Assert.Equal((string)"SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE (NOT ((P.\"Age\" = ?) OR (P.\"Age\" = ?) OR (P.\"Age\" = ?)))",
-                (string)Sql(() => Objects<Person>().Where(p => !ages.Contains(p.Age))));
+            Assert.Equal("SELECT P FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Person\" P WHERE (NOT ((P.\"Age\" = ?) OR (P.\"Age\" = ?) OR (P.\"Age\" = ?)))",
+                Sql(() => Objects<Person>().Where(p => !ages.Contains(p.Age))));
         }
     }
 }
