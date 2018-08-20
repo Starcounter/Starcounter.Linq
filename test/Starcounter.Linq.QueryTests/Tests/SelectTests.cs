dc7b8f7467132f1cf3d984cc99b7942315b2b20c
Anton Gerasimov <me@untone.net>
2018-08-20 23:40:12 +0300

    Support .Select method for single properties
    For https://github.com/Starcounter/Starcounter.Linq/issues/46

---
+++ test/Starcounter.Linq.QueryTests/Tests/SelectTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using static Starcounter.Linq.DbLinq;
+
+namespace Starcounter.Linq.QueryTests
+{
+    public class SelectTests : IClassFixture<BaseTestsFixture>
+    {
+        private readonly BaseTestsFixture _fixture;
+
+        public SelectTests(BaseTestsFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Theory]
+        [InlineData(Mode.AdHoc)]
+        [InlineData(Mode.CompiledQuery)]
+        public void SelectObjectDirectly(Mode mode)
+        {
+            Scheduling.RunTask(() =>
+            {
+                List<Person> people = mode == Mode.CompiledQuery
+                    ? CompileQuery(() => Objects<Person>().Select(x => x))().ToList()
+                    : Objects<Person>().Select(x => x).ToList();
+                Assert.Equal(2, people.Count);
+            }).Wait();
+        }
+
+        [Theory]
+        [InlineData(Mode.AdHoc)]
+        [InlineData(Mode.CompiledQuery)]
+        public void SelectObjectProperty(Mode mode)
+        {
+            Scheduling.RunTask(() =>
+            {
+                List<Office> offices = mode == Mode.CompiledQuery
+                    ? CompileQuery(() => Objects<Person>().Select(x => x.Office))().ToList()
+                    : Objects<Person>().Select(x => x.Office).ToList();
+                Assert.Equal(2, offices.Count);
+            }).Wait();
+        }
+
+        [Theory]
+        [InlineData(Mode.AdHoc)]
+        [InlineData(Mode.CompiledQuery)]
+        public void SelectStringProperty(Mode mode)
+        {
+            Scheduling.RunTask(() =>
+            {
+                List<string> names = mode == Mode.CompiledQuery
+                    ? CompileQuery(() => Objects<Person>().Select(x => x.Name))().ToList()
+                    : Objects<Person>().Select(x => x.Name).ToList();
+                Assert.Equal(2, names.Count);
+            }).Wait();
+        }
+
+        [Theory]
+        [InlineData(Mode.AdHoc)]
+        [InlineData(Mode.CompiledQuery)]
+        public void SelectIntegerProperty(Mode mode)
+        {
+            Scheduling.RunTask(() =>
+            {
+                List<int> ages = mode == Mode.CompiledQuery
+                    ? CompileQuery(() => Objects<Person>().Select(x => x.Age))().ToList()
+                    : Objects<Person>().Select(x => x.Age).ToList();
+                Assert.Equal(2, ages.Count);
+            }).Wait();
+        }
+    }
+}
---
+++ test/Starcounter.Linq.SqlTests/SelectTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Xunit;
+using static Starcounter.Linq.DbLinq;
+using static Starcounter.Linq.SqlTests.Utils;
+
+namespace Starcounter.Linq.SqlTests
+{
+    public class SelectTests
+    {
+        [Fact]
+        public void SelectObjectDirectly()
+        {
+            Assert.Equal("SELECT D FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Department\" D",
+                Sql(() => Objects<Department>().Select(x => x)));
+        }
+
+        [Fact]
+        public void SelectObjectProperty()
+        {
+            Assert.Equal("SELECT D.\"Company\" FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Department\" D",
+                Sql(() => Objects<Department>().Select(x => x.Company)));
+        }
+
+        [Fact]
+        public void SelectStringProperty()
+        {
+            Assert.Equal("SELECT D.\"Name\" FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Department\" D",
+                Sql(() => Objects<Department>().Select(x => x.Name)));
+        }
+
+        [Fact]
+        public void SelectEmbeddedProperty()
+        {
+            Assert.Equal("SELECT D.\"Company\".\"Name\" FROM \"Starcounter\".\"Linq\".\"SqlTests\".\"Department\" D",
+                Sql(() => Objects<Department>().Select(x => x.Company.Name)));
+        }
+    }
+}
